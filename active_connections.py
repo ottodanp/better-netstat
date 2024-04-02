2a89567ae8734c031551c501b1c1336f249cdf8a
ottodanp <130405700+ottodanp@users.noreply.github.com>
2024-04-02 17:13:33 +0100

    refactored active_connections.py

--- active_connections.py
+++ active_connections.py
@@ -1,23 +1,38 @@
-from net import get_netstat
+from typing import List
+
+from net import Connection, get_netstat
 from util import display_connections, display_loop, maximize_terminal
 
 DISPLAY_HEADERS = ["Protocol", "Local Address", "Local Port", "Foreign Address", "Foreign Port", "State", "PID",
                    "Process Name", "Network Relationship", "Process Started"]
 
 
-def display_callback() -> None:
-    connections = get_netstat()
-    display_connections(connections, DISPLAY_HEADERS)
+class ConnectionViewer:
+    _running: bool
+    _connections: List[Connection]
 
+    def __init__(self):
+        self._running = True
 
-def main() -> None:
-    display_loop(
-        sleep_time=5,
-        callback=display_callback,
-        condition_callback=lambda: True
-    )
+    def update_connections(self) -> None:
+        self._connections = get_netstat()
+
+    def display_callback(self) -> None:
+        display_connections(self._connections, DISPLAY_HEADERS)
+
+    def begin_display(self) -> None:
+        display_loop(
+            sleep_time=5,
+            callback=self.display_callback,
+            condition_callback=self.running
+        )
+
+    @property
+    def running(self) -> bool:
+        return self._running
 
 
 if __name__ == '__main__':
     maximize_terminal()
-    main()
+    viewer = ConnectionViewer()
+    viewer.begin_display()
